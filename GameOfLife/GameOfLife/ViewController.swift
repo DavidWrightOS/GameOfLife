27f04ee8c9496fcffd380fad361416f46acc78b1
David Wright <davidtwright89@gmail.com>
2020-08-18 03:43:43 -0400

    Created a new Xcode project

---
+++ GameOfLife/GameOfLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameOfLife
+//
+//  Created by David Wright on 8/18/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
