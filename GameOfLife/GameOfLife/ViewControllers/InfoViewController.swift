46edae4690f632089fadced3fcb4ac0a5f5f223b
David Wright <davidtwright89@gmail.com>
2020-08-19 03:49:55 -0400

    Created an InfoViewController file

---
+++ GameOfLife/GameOfLife/ViewControllers/InfoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  InfoViewController.swift
+//  GameOfLife
+//
+//  Created by David Wright on 8/19/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    
+}
