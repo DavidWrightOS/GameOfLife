3c294052d9a68b31fcd124b7af662cc13afbb834
David Wright <davidtwright89@gmail.com>
2020-08-19 03:34:22 -0400

    Created a GameViewController file

---
+++ GameOfLife/GameOfLife/ViewControllers/GameViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GameViewController.swift
+//  GameOfLife
+//
+//  Created by David Wright on 8/19/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Initializers
+    
+    
+    
+    // MARK: - IBOutlets
+    
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - IBActions
+    
+    
+    
+}
