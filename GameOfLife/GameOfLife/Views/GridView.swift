457f9d75bae9952c5d66faaaa60c1ce8c71ec200
David Wright <davidtwright89@gmail.com>
2020-08-19 04:48:32 -0400

    Added a GridView class which will be a view that can display a Grid object

---
+++ GameOfLife/GameOfLife/Views/GridView.swift
@@ -0,0 +1,13 @@
+//
+//  GridView.swift
+//  GameOfLife
+//
+//  Created by David Wright on 8/19/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class GridView: UIView {
+    
+}
