31977f4624abd69edeab76f3fc7c2895e30754d6
David Wright <davidtwright89@gmail.com>
2020-08-19 03:57:40 -0400

    Added a Cell model

---
+++ GameOfLife/GameOfLife/Models/Cell.swift
@@ -0,0 +1,13 @@
+//
+//  Cell.swift
+//  GameOfLife
+//
+//  Created by David Wright on 8/19/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import Foundation
+
+struct Cell {
+    
+}
