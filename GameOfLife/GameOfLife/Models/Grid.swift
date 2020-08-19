520691a206d2a154c3de6be012e0ba50569d7f31
David Wright <davidtwright89@gmail.com>
2020-08-19 04:04:06 -0400

    Added a Grid model

---
+++ GameOfLife/GameOfLife/Models/Grid.swift
@@ -0,0 +1,13 @@
+//
+//  Grid.swift
+//  GameOfLife
+//
+//  Created by David Wright on 8/19/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import Foundation
+
+struct Grid {
+    
+}
