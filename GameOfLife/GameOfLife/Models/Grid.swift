8d64ebfe83c29b2fcf2a0f825ce5d135afe5c59c
David Wright <davidtwright89@gmail.com>
2020-08-20 04:53:22 -0400

    Implemented the Grid model

--- GameOfLife/GameOfLife/Models/Grid.swift
+++ GameOfLife/GameOfLife/Models/Grid.swift
@@ -10,4 +10,37 @@ import Foundation
 
 struct Grid {
     
+    // MARK: - Properties
+    
+    var cells = [Cell]()
+    let width: Int
+    let height: Int
+    
+    // MARK: - Initializer
+    
+    public init(width: Int, height: Int, cells: [Cell]? = nil) {
+        self.width = width
+        self.height = height
+        
+        if let cells = cells {
+            self.cells = cells
+        } else {
+            for x in 0..<width {
+                for y in 0..<height {
+                    let randomState = Int.random(in: 0...5)
+                    let cell = Cell(x: x, y: y, state: randomState == 0 ? .alive : .dead)
+                    self.cells.append(cell)
+                }
+            }
+        }
+    }
+    
+    // MARK: - Methods
+    
+    func cellAt(x: Int, y: Int) -> Cell? {
+        guard x >= 0, x < width,
+            y >= 0, y < height else { return nil }
+        
+        return cells[y * width + x]
+    }
 }
