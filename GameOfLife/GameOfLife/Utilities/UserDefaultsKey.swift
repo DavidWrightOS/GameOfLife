d2e193ce3e0658b60af5a4c90ebf937fb578b6b8
David Wright <davidtwright89@gmail.com>
2020-09-08 23:43:36 -0400

    Added a UserDefaultsKey struct for easy access to the keys that will be used to store the user settings

---
+++ GameOfLife/GameOfLife/Utilities/UserDefaultsKey.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsKey.swift
+//  GameOfLife
+//
+//  Created by David Wright on 9/8/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+import Foundation
+
+struct UserDefaultsKey {
+    
+    private init() {}
+    
+    static let gameSpeed = "GameSpeed"
+    static let gridSize = "GridSize"
+}
